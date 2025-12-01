4bc7d40828e190d869311b9d60acb290bc67a6f3
Rushabh Doshi <doshi@mac.mynetworksettings.com>
2025-12-01 17:21:10 -0500

    part 2 done

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ public:
             if (turn == 'R') {
                 curr = positive_dial(curr, value);
             }
-            else if (turn == 'L') {
+            if (turn == 'L') {
                 curr = negative_dial(curr, value);
             }
 
@@ -44,8 +44,32 @@ public:
     }
 
     int part_two() {
+        int curr = 50;
         int count = 0;
+        std::ifstream file("input.txt");
+        std::string line;
 
+        int n_clicks = 0;
+        while (std::getline(file, line)) {
+            if (line.empty()) continue;
+
+            char turn = line[0]; // L or R
+            int value = std::stoi(line.substr(1)); // the number
+            // std::cout << turn << " : " << value << std::endl;
+            
+            for (int i = 0; i < value; i++) {
+                if (turn == 'R') {
+                    curr = positive_dial(curr, 1);
+                }
+                if (turn == 'L') {
+                    curr = negative_dial(curr, 1);
+                }
+                
+                count += (curr == 0) ? 1 : 0;
+            }
+        }
+
+        cout << count << endl;
         return count;
     }
 };
@@ -53,7 +77,8 @@ public:
 
 int main() {
     Solution solution;
-    solution.part_one();
+    // solution.part_one();
+    solution.part_two();
 
     return 0;
 }
